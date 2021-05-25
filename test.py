7a56f16c6fb168cd5a99a80f68912d842b957f95
AndreiBaetica1 <83407799+AndreiBaetica1@users.noreply.github.com>
2021-05-25 14:33:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import socketserver
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = 'mywebpage1.html'
+            self.path = 'mywebpage2.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 # Create an object of the above class
 handler_object = MyHttpRequestHandler
